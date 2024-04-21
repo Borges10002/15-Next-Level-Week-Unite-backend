ff7472849a7a8a748d66956491b2eba288a1551c
Borges10002 <borges10002@gmail.com>
2024-04-21 07:08:52 -0400

    class number fifteen

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import fastify from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+import { createEvent } from "./routes/create-event";
+
+const app = fastify();
+
+// Add schema validator and serializer
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createEvent);
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("HTTP server running!");
+});
